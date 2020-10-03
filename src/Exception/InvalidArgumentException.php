0eb1725052b97f26f125651bc92f1aa6e2004fe2
Chris Yates <beastbytes@users.noreply.github.com>
2020-10-03 18:42:20 +0100

    Create InvalidArgumentException.php

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright Copyright &copy; 2020 BeastBytes - All Rights Reserved
+ * @license https://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ * @link https://www.yiiframework.com/
+ * @link https://github.com/beastbytes/yii3-webpackmanifestasset
+ */
+
+declare(strict_types=1);
+
+namespace BeastBytes\WebpackManifestAsset\Exception;
+
+/**
+ * Class InvalidConfigException.
+ */
+class InvalidConfigException extends \Exception
+{
+}
